c6275155917d391ac980951851b4a14c4b0786ad
Matik Laptop <mateuszkowaslki199700@gmail.com>
2022-11-03 06:07:17 +0100

    Tik Tac Toe - 90% done Choose places, Reset button, Back button (base on dynamic buttons in grid)

--- app/src/main/java/com/example/arcade_machine/ActivityTicTacToe.java
+++ app/src/main/java/com/example/arcade_machine/ActivityTicTacToe.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -12,8 +14,12 @@ import java.util.ArrayList;
 
 public class ActivityTicTacToe extends AppCompatActivity {
 
-    Button btnBack;
+    ArrayList<Button> buttons = new ArrayList<>();
+
     GridLayout grid;
+
+    Button btnBack;
+    Button btnReset;
 
     Integer player = 0;
 
@@ -22,71 +28,107 @@ public class ActivityTicTacToe extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
 
+        View view = findViewById(android.R.id.content);
+
         btnBack = findViewById(R.id.btnBack);
+        btnReset = findViewById(R.id.btnReset);
+
+        grid = findViewById(R.id.grid);
+
+//        buttons.add(findViewById(R.id.btn1));
+//        buttons.add(findViewById(R.id.btn2));
+//        buttons.add(findViewById(R.id.btn3));
+//        buttons.add(findViewById(R.id.btn4));
+//        buttons.add(findViewById(R.id.btn5));
+//        buttons.add(findViewById(R.id.btn6));
+//        buttons.add(findViewById(R.id.btn7));
+//        buttons.add(findViewById(R.id.btn8));
+//        buttons.add(findViewById(R.id.btn9));
+
+//        for (Button button : buttons) {
+//            button.setOnClickListener(v -> {
+//                if (button.getText().toString().equals(""))
+//                    button.setText( (player++%2 == 0) ? "X" : "O");
+//                else
+//                    Toast.makeText(this, "This place is already taken", Toast.LENGTH_SHORT).show();
+//                if (checkBoard()){
+//                    Toast.makeText(this, "Player " + (player%2 == 0 ? "O" : "X") + " won!", Toast.LENGTH_SHORT).show();
+//                    for (Button b : buttons){
+//                        b.setEnabled(false);
+//
+//                    }
+//                }
+//            });
+//        }
 
         btnBack.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
+        btnReset.setOnClickListener(v -> play(view));
 
-        play();
+        play(view);
     }
 
-    private void play() {
+    private void play(View view) {
         player = 0;
-        grid = findViewById(R.id.grid);
         grid.removeAllViews();
+        buttons.clear();
         for (int i = 0; i < 9; i++) {
             Button btn = new Button(this);
-            btn.setText(" ");
+
+            btn.setText("");
+            btn.setWidth(200);
+            btn.setHeight(200);
+            btn.setTextSize(30);
+            btn.setTextAppearance(R.style.PressStart2P);
 
             btn.setOnClickListener(v -> {
-                if(btn.getText() == " ")
-                    btn.setText((player++%2==0) ? "X" : "O");
+                if (btn.getText().toString().equals(""))
+                    btn.setText( (player++%2 == 0) ? "X" : "O");
                 else
-                    Toast.makeText(this, "This field is already taken", Toast.LENGTH_SHORT).show();
-
-                checkBoard();
+                    Toast.makeText(this, "This place is already taken", Toast.LENGTH_SHORT).show();
+                int [] win = checkBoard();
+                if (win.length > 0){
+                    Toast.makeText(this, "Player " + (player%2 == 0 ? "O" : "X") + " won!", Toast.LENGTH_SHORT).show();
+                    for (Button b : buttons){
+                        b.setEnabled(false);
+                    }
+//                    draw line from win[0] to win[2]
+                    int [][] pos = {{0,0}, {0,0}};
+                    buttons.get(win[0]).getLocationOnScreen(pos[0]);
+                    buttons.get(win[2]).getLocationOnScreen(pos[1]);
+                    System.out.println(pos[0][0] + " " + pos[0][1]);
+                    System.out.println(pos[1][0] + " " + pos[1][1]);
+                }
+                else if (player == 9){
+                    Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+                }
             });
             grid.addView(btn);
+            buttons.add(btn);
         }
-        grid.refreshDrawableState();
     }
 
-    private void checkBoard() {
-        ArrayList<Button> buttons = new ArrayList<>();
-        for (int i = 0; i < grid.getChildCount(); i++) {
-            buttons.add((Button) grid.getChildAt(i));
-        }
-
-        if (player == 9){
-            Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if ( buttons.get(4).getText() != " " && (
+    private int[] checkBoard() {
+        if (!buttons.get(4).getText().toString().equals("") &&
             buttons.get(0).getText() == buttons.get(4).getText() &&
-            buttons.get(0).getText() == buttons.get(8).getText() ||
+            buttons.get(0).getText() == buttons.get(8).getText()
+        ) return new int[]{0, 4, 8};
+        if (!buttons.get(4).getText().toString().equals("") &&
             buttons.get(2).getText() == buttons.get(4).getText() &&
             buttons.get(2).getText() == buttons.get(6).getText()
-        ) ) {
-            Toast.makeText(this, "Player " + buttons.get(4).getText() + " won!", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        ) return new int[]{2, 4, 6};
         for (int i = 0; i < 3; i++) {
-            if ( buttons.get(i).getText() != " " &&
+            if (!buttons.get(i).getText().toString().equals("") &&
                 buttons.get(i).getText() == buttons.get(i+3).getText() &&
                 buttons.get(i).getText() == buttons.get(i+6).getText()
-            ) {
-                Toast.makeText(this, buttons.get(i).getText() + " won", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            if ( buttons.get(i*3).getText() != " " &&
+            ) return new int[]{i, i+3, i+6};
+            if (!buttons.get(i*3).getText().toString().equals("") &&
                 buttons.get(i*3).getText() == buttons.get(i*3+1).getText() &&
                 buttons.get(i*3).getText() == buttons.get(i*3+2).getText()
-            ) {
-                Toast.makeText(this, buttons.get(i*3).getText() + " won", Toast.LENGTH_SHORT).show();
-                return;
-            }
+            ) return new int[]{i*3, i*3+1, i*3+2};
         }
+        return new int[]{};
     }
 }
